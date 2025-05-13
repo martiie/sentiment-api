7995e446b15471bc347c7539f21b8dc42619e3bd
martiie <136836097+martiie@users.noreply.github.com>
2025-05-13 13:38:38 +0700

    Update predict_api.py

--- predict_api.py
+++ predict_api.py
@@ -12,14 +12,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-model, vectorizer = joblib.load("model/sentiment_model.pkl")
+pipeline = joblib.load("model/sentiment_model.pkl")
 
 class Review(BaseModel):
     text: str
 
 @app.post("/predict")
 def predict_sentiment(review: Review):
-    vec = vectorizer.transform([review.text])
-    pred = model.predict(vec)[0]
+    pred = pipeline.predict([review])[0]
     label = "positive" if pred == 1 else "negative"
     return {"sentiment": label}
