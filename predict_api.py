912473c8354eb771e8be89fe2d0904c27c6814b6
martiie <136836097+martiie@users.noreply.github.com>
2025-05-13 12:30:16 +0700

    Update predict_api.py

--- predict_api.py
+++ predict_api.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import joblib
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # หรือ ["*"] เพื่อให้ทุก origin เข้าถึงได้ (ใช้เฉพาะช่วง dev)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 model, vectorizer = joblib.load("model/sentiment_model.pkl")
 
