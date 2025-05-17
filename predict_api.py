8b312821707047e8aa4da82bd4cdbe8634fc7be0
martiie <136836097+martiie@users.noreply.github.com>
2025-05-17 12:38:30 +0700

    Update predict_api.py

--- predict_api.py
+++ predict_api.py
@@ -26,7 +26,10 @@ lr = joblib.load('model/logistic_model.pkl')
 cvec = joblib.load('model/count_vectorizer.pkl')
 class Review(BaseModel):
     text: str
-
+    
+def custom_analyzer(text):
+    return text.split(' ')
+    
 def text_process(text):
     final = "".join(u for u in text if u not in ("?", ".", ";", ":", "!", '"', "ๆ", "ฯ"))
     final = word_tokenize(final)
